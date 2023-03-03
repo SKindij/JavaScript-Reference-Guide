3795470549be928e2fd47ef2fac0fbfb2af50416
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-03 22:02:54 +0200

    Update DOM-manipulation.js

--- DOM-BOM-section/DOM-manipulation.js
+++ DOM-BOM-section/DOM-manipulation.js
@@ -2,8 +2,15 @@ const newSection = document.createElement('section');
 const newDiv = document.createElement('div');
 
 document.body.appendChild(newSection); //=> <section> </section>
-  let mySection = document.querySelector("section"); 
-    mySection.appendChild(newDiv); //=> <div> </div>
+let staticSection = document.querySelector("section"); 
+
+staticSection.appendChild(newDiv); //=> <div> </div>
+let staticDiv = document.querySelector("div");
+  
+staticSection.className = "static";
+staticDiv.className = "hacked";
+
+
 
 
 
