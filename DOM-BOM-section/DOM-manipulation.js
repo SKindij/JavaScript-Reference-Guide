4f5c91f0d66ca937325a75463a4d106647c0151c
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-03 21:43:58 +0200

    Update and rename DOM-manipulation to DOM-manipulation.js

--- DOM-BOM-section/DOM-manipulation.js
+++ DOM-BOM-section/DOM-manipulation.js
@@ -1,3 +1,9 @@
 const newSection = document.createElement('section');
 const newElementA = document.createElement('div');
 const newElementB = document.createElement('div');
+
+document.body.appendChild(newSection);
+
+
+
+
